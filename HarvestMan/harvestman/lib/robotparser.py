9f86ae009f1e74ee040c0d6e683bcba6894a674b
Anand B Pillai <pythonhacker@users.noreply.github.com>
2009-02-23 13:15:58 +0000

    Fix for robotparser

--- HarvestMan/harvestman/lib/robotparser.py
+++ HarvestMan/harvestman/lib/robotparser.py
@@ -28,8 +28,8 @@
 
     Jan 8 2006         Anand    Updated this file from EIAO robacc
                                 repository.
-    Jan 10 2006          Anand   Converted from dos to unix format (removed Ctrl-Ms).
-                                
+    Jan 10 2006        Anand   Converted from dos to unix format (removed Ctrl-Ms).
+    Feb 23 2009        Anand    Updated module contents from Python 2.5.                
 
 """
 
@@ -61,83 +61,88 @@ def _debug(msg):
 
 import socket
 
-class RobotFileParser(object):
+class RobotFileParser:
+    """ This class provides a set of methods to read, parse and answer
+    questions about a single robots.txt file.
+
+    """
+
     def __init__(self, url=''):
         self.entries = []
-        self.disallow_all = 0
-        self.allow_all = 0
+        self.default_entry = None
+        self.disallow_all = False
+        self.allow_all = False
         self.set_url(url)
         self.last_checked = 0
-        # value of last return value
-        # of can_fetch() method
-        self.cache_val = 0
-        # value of url in last call
-        # to can_fetch() method
-        self.directory = ''
 
     def mtime(self):
+        """Returns the time the robots.txt file was last fetched.
+
+        This is useful for long-running web spiders that need to
+        check for new robots.txt files periodically.
+
+        """
         return self.last_checked
 
     def modified(self):
+        """Sets the time the robots.txt file was last fetched to the
+        current time.
+
+        """
         import time
         self.last_checked = time.time()
 
     def set_url(self, url):
+        """Sets the URL referring to a robots.txt file."""
         self.url = url
         self.host, self.path = urlparse.urlparse(url)[1:3]
 
-    def get_server(self):
-        urltuple = urlparse.urlparse(self.url)
-        return urltuple[1]
-    
     def read(self):
-        
+        """Reads the robots.txt URL and feeds it to the parser."""
         opener = URLopener()
         f = opener.open(self.url)
-        # bug: we need to check this value against '0'
-        if f is None: return -1
-        try:
-            lines = f.readlines()
-        except AttributeError, e:
-            return 0
-        except socket.error, e:
-            print e
-            return 0
-        
+        lines = []
+        line = f.readline()
+        while line:
+            lines.append(line.strip())
+            line = f.readline()
         self.errcode = opener.errcode
         if self.errcode == 401 or self.errcode == 403:
-            self.disallow_all = 1
-            _moredebug("disallow all")
+            self.disallow_all = True
+            _debug("disallow all")
         elif self.errcode >= 400:
-            self.allow_all = 1
-            _moredebug("allow all")
+            self.allow_all = True
+            _debug("allow all")
         elif self.errcode == 200 and lines:
-            _moredebug("parse lines")
+            _debug("parse lines")
             self.parse(lines)
 
-        return 0
+    def _add_entry(self, entry):
+        if "*" in entry.useragents:
+            # the default entry is considered last
+            self.default_entry = entry
+        else:
+            self.entries.append(entry)
 
     def parse(self, lines):
-        """parse the input lines from a robot.txt file.
+        """parse the input lines from a robots.txt file.
            We allow that a user-agent: line is not preceded by
            one or more blank lines."""
-           
         state = 0
         linenumber = 0
         entry = Entry()
 
         for line in lines:
-            line = line.strip()
             linenumber = linenumber + 1
             if not line:
                 if state==1:
-                    _verbosedebug("line %d: warning: you should insert"
+                    _debug("line %d: warning: you should insert"
                            " allow: or disallow: directives below any"
                            " user-agent: line" % linenumber)
                     entry = Entry()
                     state = 0
                 elif state==2:
-                    self.entries.append(entry)
+                    self._add_entry(entry)
                     entry = Entry()
                     state = 0
             # remove optional comment and strip line
@@ -150,63 +155,58 @@ class RobotFileParser(object):
             line = line.split(':', 1)
             if len(line) == 2:
                 line[0] = line[0].strip().lower()
-                line[1] = line[1].strip()
+                line[1] = urllib.unquote(line[1].strip())
                 if line[0] == "user-agent":
                     if state==2:
-                        _verbosedebug("line %d: warning: you should insert a blank"
+                        _debug("line %d: warning: you should insert a blank"
                                " line before any user-agent"
                                " directive" % linenumber)
-                        self.entries.append(entry)
+                        self._add_entry(entry)
                         entry = Entry()
                     entry.useragents.append(line[1])
                     state = 1
                 elif line[0] == "disallow":
                     if state==0:
-                        _verbosedebug("line %d: error: you must insert a user-agent:"
+                        _debug("line %d: error: you must insert a user-agent:"
                                " directive before this line" % linenumber)
                     else:
-                        entry.rulelines.append(RuleLine(line[1], 0))
+                        entry.rulelines.append(RuleLine(line[1], False))
                         state = 2
                 elif line[0] == "allow":
                     if state==0:
-                        _verbosedebug("line %d: error: you must insert a user-agent:"
+                        _debug("line %d: error: you must insert a user-agent:"
                                " directive before this line" % linenumber)
                     else:
-                        entry.rulelines.append(RuleLine(line[1], 1))
+                        entry.rulelines.append(RuleLine(line[1], True))
                 else:
-                    _verbosedebug("line %d: warning: unknown key %s" % (linenumber,
+                    _debug("line %d: warning: unknown key %s" % (linenumber,
                                line[0]))
             else:
-                _verbosedebug("line %d: error: malformed line %s"%(linenumber, line))
+                _debug("line %d: error: malformed line %s"%(linenumber, line))
         if state==2:
             self.entries.append(entry)
-        _moredebug("Parsed rules:\n%s" % str(self))
+        _debug("Parsed rules:\n%s" % str(self))
 
-    def get_directory(self):
-        return self.directory
-    
+
     def can_fetch(self, useragent, url):
         """using the parsed robots.txt decide if useragent can fetch url"""
-        _moredebug("Checking robot.txt allowance for:\n  user agent: %s\n  url: %s" %
+        _debug("Checking robots.txt allowance for:\n  user agent: %s\n  url: %s" %
                (useragent, url))
-        # cache value
-        self.directory = url
         if self.disallow_all:
-            self.cache_val = 0
-            return 0
+            return False
         if self.allow_all:
-            self.cache_val = 1
-            return 1
+            return True
         # search for given user agent matches
         # the first match counts
-        url = urllib.quote(urlparse.urlparse(url)[2]) or "/"
-        
+        url = urllib.quote(urlparse.urlparse(urllib.unquote(url))[2]) or "/"
         for entry in self.entries:
             if entry.applies_to(useragent):
                 return entry.allowance(url)
+        # try the default entry last
+        if self.default_entry:
+            return self.default_entry.allowance(url)
         # agent not found ==> access granted
-        self.cache_val = 1
-        return 1
+        return True
 
 
     def __str__(self):
@@ -216,21 +216,24 @@ class RobotFileParser(object):
         return ret
 
 
-class RuleLine(object):
-    """A rule line is a single "Allow:" (allowance==1) or "Disallow:"
-       (allowance==0) followed by a path."""
+class RuleLine:
+    """A rule line is a single "Allow:" (allowance==True) or "Disallow:"
+       (allowance==False) followed by a path."""
     def __init__(self, path, allowance):
+        if path == '' and not allowance:
+            # an empty value means allow all
+            allowance = True
         self.path = urllib.quote(path)
         self.allowance = allowance
 
     def applies_to(self, filename):
-        return self.path=="*" or re.match(self.path, filename)
+        return self.path=="*" or filename.startswith(self.path)
 
     def __str__(self):
         return (self.allowance and "Allow" or "Disallow")+": "+self.path
 
 
-class Entry(object):
+class Entry:
     """An entry has one or more user-agents and zero or more rulelines"""
     def __init__(self):
         self.useragents = []
@@ -251,72 +254,55 @@ class Entry(object):
         for agent in self.useragents:
             if agent=='*':
                 # we have the catch-all agent
-                return 1
+                return True
             agent = agent.lower()
-            # don't forget to re.escape
-            if re.search(re.escape(useragent), agent):
-                return 1
-        return 0
+            if agent in useragent:
+                return True
+        return False
 
     def allowance(self, filename):
         """Preconditions:
         - our agent applies to this entry
-        - filename is URLdecoded"""
+        - filename is URL decoded"""
         for line in self.rulelines:
-            _verbosedebug((filename, str(line), line.allowance))
+            _debug((filename, str(line), line.allowance))
             if line.applies_to(filename):
                 return line.allowance
-        return 1
+        return True
 
 class URLopener(urllib.FancyURLopener):
-    
     def __init__(self, *args):
-        apply(urllib.FancyURLopener.__init__, (self,) + args)
+        urllib.FancyURLopener.__init__(self, *args)
         self.errcode = 200
-        self.tries = 0
-        self.maxtries = 10
 
     def http_error_default(self, url, fp, errcode, errmsg, headers):
         self.errcode = errcode
         return urllib.FancyURLopener.http_error_default(self, url, fp, errcode,
                                                         errmsg, headers)
 
-    def http_error_302(self, url, fp, errcode, errmsg, headers, data=None):
-        self.tries += 1
-        if self.tries >= self.maxtries:
-            return self.http_error_default(url, fp, 500,
-                                           "Internal Server Error: Redirect Recursion",
-                                           headers)
-        result = urllib.FancyURLopener.http_error_302(self, url, fp, errcode,
-                                                      errmsg, headers, data)
-        self.tries = 0
-        return result
-
     def open(self, url):
         return HarvestManUrlConnector().robot_urlopen(url)
-    
+
 def _check(a,b):
     if not b:
         ac = "access denied"
     else:
         ac = "access allowed"
     if a!=b:
-        _basicdebug("failed")
+        print "failed"
     else:
-        _basicdebug("ok (%s)" % ac)
-    _basicdebug('')
+        print "ok (%s)" % ac
+    print
 
 def _test():
     global debug
     rp = RobotFileParser()
-    debug = 2
+    debug = 1
 
     # robots.txt that exists, gotten to by redirection
     rp.set_url('http://www.musi-cal.com/robots.txt')
-    #rp.set_url('http://www.lycos.com/robots.txt')
     rp.read()
 
-    return
     # test for re.escape
     _check(rp.can_fetch('*', 'http://www.musi-cal.com/'), 1)
     # this should match the first rule, which is a disallow
@@ -342,15 +328,10 @@ def _test():
     _check(rp.can_fetch('spam', 'http://www.musi-cal.com/'), 1)
     _check(rp.can_fetch('spam', 'http://www.musi-cal.com/'), 1)
 
-    rp.set_url('http://members.fortunecity.com/robots.txt')
-    rp.read()
-    _check(rp.can_fetch('Python urllib2 module', 'http://www.fortunecity.com/login.shtml'), 1)
-    
     # robots.txt that does not exist
     rp.set_url('http://www.lycos.com/robots.txt')
     rp.read()
     _check(rp.can_fetch('Mozilla', 'http://www.lycos.com/search'), 1)
-    
 
 if __name__ == '__main__':
     _test()
